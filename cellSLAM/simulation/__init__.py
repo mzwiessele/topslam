bbf7495675b5e92b80bec22e26cb4fd954baf7a5
mzwiessele <ibinbei@gmail.com>
2016-04-13 11:13:12 +0100

    [optimization] added optimization filtering and cleanup examples are still todo

--- cellSLAM/simulation/__init__.py
+++ cellSLAM/simulation/__init__.py
@@ -28,7 +28,7 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #===============================================================================
 
-from . import simulate_latent_space, simulate_new_Y, guo_simulation
+from .simulate_trajectory import simulate_latent_space, simulate_new_Y, guo_simulation
 
 import numpy as np
 
