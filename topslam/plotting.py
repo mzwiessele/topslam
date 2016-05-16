9496d2baacc510175515f04af0ea1e8a71794779
mzwiessele <ibinbei@gmail.com>
2016-05-16 07:53:45 +0100

    [plotting] scatter other

--- topslam/plotting.py
+++ topslam/plotting.py
@@ -30,7 +30,7 @@
 
 from matplotlib import pyplot as plt
 from scipy.spatial.distance import squareform
-import numpy as np
+import numpy as np, itertools
 from .pseudo_time.distance_correction import _get_label_pos, _get_colors
 from GPy.plotting.gpy_plot.plot_util import find_best_layout_for_subplots
 
@@ -41,6 +41,45 @@ def plot_dist_hist(M, ax=None):
     D = np.einsum('ijq,ijq->ij',d_m,d_m)
     _ = ax.hist(squareform(D), bins=200)
 
+def plot_landscape_other(X, pt, labels, ulabels, ax=None, cmap='magma', cmap_index=None, box=True, 
+                         text_kwargs=None, adjust=True, 
+                         adjust_kwargs=dict(arrowprops=dict(arrowstyle="fancy",
+                                                            fc=".6", ec="none",
+                                                            ),
+                                            ha='center', va='center', force_text=.5, precision=.5),
+                         **scatter_kwargs):
+    if ax is None:
+        _, ax = plt.subplots()
+    label_pos, col, mi, ma = _get_label_pos(X, pt, labels, ulabels)
+    colors = _get_colors(cmap, col, mi, ma, cmap_index)
+    marker = itertools.cycle('<>sd^')
+    texts = []
+    for l in ulabels:
+        #c = Tango.nextMedium()
+        c, r = colors[l]
+        p = label_pos[l]
+        fil = (labels==l)
+        ax.scatter(*X[fil].T, linewidth=.1, facecolor=c, alpha=.8, edgecolor='w', 
+                   marker=next(marker), label=l, **scatter_kwargs)
+        rgbc = c#[_c/255. for _c in Tango.hex2rgb(c)]
+        if r <.5:
+            ec = 'w'
+        else:
+            ec = 'k'
+        if box:
+            fc = list(rgbc)
+            #fc[-1] = .7
+            props = dict(boxstyle='round', facecolor=fc, alpha=0.6, edgecolor=ec, pad=0.2)
+        else:
+            props = dict()
+        texts.append(ax.text(p[0], p[1], l, alpha=.9, ha='center', va='center', color=ec, bbox=props, **text_kwargs or {}))
+    xlim, ylim = ax.get_xlim(), ax.get_ylim()
+    x1, y1 = np.mgrid[xlim[0]:xlim[1]:100j,ylim[0]:ylim[1]:2j]
+    x2, y2 = np.mgrid[xlim[0]:xlim[1]:2j,ylim[0]:ylim[1]:100j]
+    x, y = np.r_[x1[:,0], x2[1], x1[::-1,1], x2[0]], np.r_[y1[:,0], y2[1], y1[:,1], y2[0,::-1]]
+    from adjustText import adjust_text
+    adjust_text(texts, x, y, ax=ax, **adjust_kwargs)
+    return ax
 
 def plot_comparison(mc, X_init, dims, labels, ulabels, start, cmap='magma', 
                     cmap_index=None, box=True, text_kwargs=None, 
@@ -92,42 +131,14 @@ def plot_comparison(mc, X_init, dims, labels, ulabels, start, cmap='magma',
     ax.text(0.01,.98,"topslam",va='top',transform=ax.transAxes,color='w')
 
     pt = mc.get_pseudo_time(start=start)
-    import itertools
-    from adjustText import adjust_text
     
     i = 0
     for name in dims:
         ax = next(axit)
         X = X_init[:,dims[name]]
-        label_pos, col, mi, ma = _get_label_pos(X, pt, labels, ulabels)
-        colors = _get_colors(cmap, col, mi, ma, cmap_index)
-        marker = itertools.cycle('<>sd^')
-        texts = []
-        for l in ulabels:
-            #c = Tango.nextMedium()
-            c, r = colors[l]
-            p = label_pos[l]
-            fil = (labels==l)
-            ax.scatter(*X[fil].T, linewidth=.1, facecolor=c, alpha=.8, edgecolor='w', 
-                       marker=next(marker), label=l, **scatter_kwargs)
-            rgbc = c#[_c/255. for _c in Tango.hex2rgb(c)]
-            if r <.5:
-                ec = 'w'
-            else:
-                ec = 'k'
-            if box:
-                fc = list(rgbc)
-                #fc[-1] = .7
-                props = dict(boxstyle='round', facecolor=fc, alpha=0.6, edgecolor=ec, pad=0.2)
-            else:
-                props = dict()
-            texts.append(ax.text(p[0], p[1], l, alpha=.9, ha='center', va='center', color=ec, bbox=props, **text_kwargs or {}))
-        xlim, ylim = ax.get_xlim(), ax.get_ylim()
-        x1, y1 = np.mgrid[xlim[0]:xlim[1]:100j,ylim[0]:ylim[1]:2j]
-        x2, y2 = np.mgrid[xlim[0]:xlim[1]:2j,ylim[0]:ylim[1]:100j]
-        x, y = np.r_[x1[:,0], x2[1], x1[::-1,1], x2[0]], np.r_[y1[:,0], y2[1], y1[:,1], y2[0,::-1]]
-        adjust_text(texts, x, y, ax=ax, **adjust_kwargs)
-        ax.text(0.01,.98,name,va='top',transform=ax.transAxes)
+        plot_landscape_other(X, pt, labels, ulabels, ax, cmap, 
+                             cmap_index, box, text_kwargs, 
+                             adjust, adjust_kwargs, **scatter_kwargs)
         i += 2
 
 
